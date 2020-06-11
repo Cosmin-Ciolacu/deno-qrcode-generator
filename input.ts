cb85fa7c1da184a6c5cd374b51570bffc0c3107d
Cosmin Ciolacu <cosmin.ciolacu@dev51.io>
2020-06-11 17:59:10 +0300

    validare

---
+++ input.ts
@@ -0,0 +1,18 @@
+async function input(
+  question: string = "",
+  stdin = Deno.stdin,
+  stdout = Deno.stdout,
+) {
+  const buf = new Uint8Array(1024);
+
+  // Write question to console
+  await stdout.write(new TextEncoder().encode(question));
+
+  // Read console's input into answer
+  const n = <number> await stdin.read(buf);
+  const answer = new TextDecoder().decode(buf.subarray(0, n));
+
+  return answer.trim();
+}
+
+export { input };
